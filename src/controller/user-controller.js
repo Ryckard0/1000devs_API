ad07ec8c81280859074f610eb9c42b1379cad8a9
Ricardo Sousa <ricardohenrique0228@gmail.com>
2023-12-05 21:38:41 -0300

    feat: api de usuarios e idade

---
+++ src/controller/user-controller.js
@@ -0,0 +1,16 @@
+const calculaIdade = (req, res) => {
+    const hoje = new Date(Date.now())
+    const nascimento = new Date("2002-03-19")
+    const idade = hoje.getFullYear() - nascimento.getFullYear()
+
+    res.json({
+        "nome": "Ricardo",
+        "cidade": "Monte Mor",
+        "profissao": "Programador",
+        "nascimento": idade
+    })
+}
+
+module.exports = {
+    calculaIdade
+}
